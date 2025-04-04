f773340b3b445af5914a380f0aef17b7085fc84a
myeongjehwang <100746681+Hwangmyeongje@users.noreply.github.com>
2025-04-04 18:26:05 +0900

    [Silver II] Title: 아기 상어 2, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
@@ -1,32 +1,38 @@
 from collections import deque
 import sys
 input = sys.stdin.readline
+
 n,m = map(int,input().split())
+
 arr = [list(map(int,input().split())) for _ in range(n)]
 
-# 상 하 좌 우 좌상 우상  좌하 우하
-dx = [0,0,-1,1,-1,1,-1,1]
-dy = [-1,1,0,0,-1,-1,1,1]
+deq = deque()
 
-shark = deque()
 for i in range(n):
     for j in range(m):
-        if arr[i][j] ==1:
-            shark.append((i,j))
+        if arr[i][j] == 1:
+            deq.append((i,j))
+# 좌상 우상 상 좌하 우하 하 좌 우
+dx = [-1,-1,-1,1,1,1,0,0]
+dy = [-1,1,0,-1,1,0,-1,1]
+
 
 def bfs():
-    while shark:
-        x,y = shark.popleft()
-        for k in range(8):
-            nx = x + dx[k]
-            ny = y + dy[k]
+    while deq:
+        x,y = deq.popleft()
+        for i in range(8):
+            nx = x+dx[i]
+            ny = y+dy[i]
             if 0<=nx<n and 0<=ny<m:
-                if arr[nx][ny] ==0:
-                    shark.append((nx,ny))
-                    arr[nx][ny] = arr[x][y]+1
+                if arr[nx][ny] == 0:
+                    arr[nx][ny] = arr[x][y] +1
+                    deq.append((nx,ny))
+
 bfs()
-safe_dist = 0
+
+maxnum = 0
 for i in range(n):
     for j in range(m):
-        safe_dist = max(safe_dist,arr[i][j])
-print(safe_dist-1)
+        maxnum = max(maxnum,arr[i][j])
+
+print(maxnum - 1)
